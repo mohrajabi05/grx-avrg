b8f578e4558d149b0bfb52d9fb936224ece15d87
mohammad rajabi <65049802+mohrajabi05@users.noreply.github.com>
2021-01-17 20:37:33 +0330

    main code

---
+++ grx-avrg.py
@@ -0,0 +1,115 @@
+def avg(*args):
+	'''index numbers and return avrage of numbers'''
+	sum = 0
+	len = 0
+	for num in args:
+		len += 1
+		sum += num
+	return sum / len
+#===================
+#index and calculate :
+numbers = int(input('tedade aadad : '))
+if numbers == 2 :
+	a = float(input('1 : '))
+	b = float(input('2 : '))
+	print(avg(a,b))
+if numbers == 3 :
+	a = float(input('1 : '))
+	b = float(input('2 : '))
+	c = float(input('3 : '))
+	print(avg(a,b,c))
+if numbers == 4 :
+	a = float(input('1 : '))
+	b = float(input('2 : '))
+	c = float(input('3 : '))
+	d = float(input('4 : ')) 
+	print(avg(a,b,c,d))
+if numbers == 5 :
+	a = float(input('1 : '))
+	b = float(input('2 : '))
+	c = float(input('3 : '))
+	d = float(input('4 : '))
+	e = float(input('5 : '))
+	print(avg(a,b,c,d,e))
+if numbers == 6 :
+	a = float(input('1 : '))
+	b = float(input('2 : '))
+	c = float(input('3 : '))
+	d = float(input('4 : '))
+	e = float(input('5 : '))
+	f = float(input('6 : '))
+	print(avg(a,b,c,d,e,f))
+if numbers == 6 :
+	a = float(input('1 : '))
+	b = float(input('2 : '))
+	c = float(input('3 : '))
+	d = float(input('4 : '))
+	e = float(input('5 : '))
+	f = float(input('6 : '))
+	g = float(input('7 : '))
+	print(avg(a,b,c,d,e,f,g))
+if numbers == 8 :
+	a = float(input('1 : '))
+	b = float(input('2 : '))
+	c = float(input('3 : '))
+	d = float(input('4 : '))
+	e = float(input('5 : '))
+	f = float(input('6 : '))
+	g = float(input('7 : '))
+	h = float(input('8 : '))
+	print(avg(a,b,c,d,e,f,g,h))
+if numbers == 9 :
+	a = float(input('1 : '))
+	b = float(input('2 : '))
+	c = float(input('3 : '))
+	d = float(input('4 : '))
+	e = float(input('5 : '))
+	f = float(input('6 : '))
+	g = float(input('7 : '))
+	h = float(input('8 : '))
+	i = float(input('9 : '))
+	print(avg(a,b,c,d,e,f,g,h,i))
+if numbers == 10 :
+	a = float(input('1 : '))
+	b = float(input('2 : '))
+	c = float(input('3 : '))
+	d = float(input('4 : '))
+	e = float(input('5 : '))
+	f = float(input('6 : '))
+	g = float(input('7 : '))
+	h = float(input('8 : '))
+	i = float(input('9 : '))
+	j = float(input('10 : '))
+	print(avg(a,b,c,d,e,f,g,h,i,j))
+if numbers == 11 :
+	a = float(input('1 : '))
+	b = float(input('2 : '))
+	c = float(input('3 : '))
+	d = float(input('4 : '))
+	e = float(input('5 : '))
+	f = float(input('6 : '))
+	g = float(input('7 : '))
+	h = float(input('8 : '))
+	i = float(input('9 : '))
+	j = float(input('10 : '))
+	k = float(input('11 : '))
+	print(avg(a,b,c,d,e,f,g,h,i,j,k))
+if numbers == 12 :
+	a = float(input('1 : '))
+	b = float(input('2 : '))
+	c = float(input('3 : '))
+	d = float(input('4 : '))
+	e = float(input('5 : '))
+	f = float(input('6 : '))
+	g = float(input('7 : '))
+	h = float(input('8 : '))
+	i = float(input('9 : '))
+	j = float(input('10 : '))
+	k = float(input('11 : '))
+	l = float(input('12 : '))
+	print(avg(a,b,c,d,e,f,g,h,i,j,k,l))
+
+
+
+#my Persian weblog: rajabi-blog.ir
+#my Email: moh.rajabi05@gmail.com
